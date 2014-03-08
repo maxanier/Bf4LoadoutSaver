16d172726f8de89872a957658766e22ba8df87bc
Maxanier <maxanier@gmx.de>
2014-03-08 21:25:55 +0100

    Adjusted instructions

--- src/de/maxgb/loadoutsaver/util/Constants.java
+++ src/de/maxgb/loadoutsaver/util/Constants.java
@@ -50,7 +50,7 @@ public class Constants {
 	public static final String BJSON_PLATFORMINT="platformInt";
 	
 	//Instructions
-	public static final Instruction INSTRUCTION_OPTIONS=new Instruction("Loadout Saver",5,"<html><body>1. Enter your Battlog login information in the options menu, so the app can edit your loadout<br>2. Use the +-Button to save your currently equipped Loadout and choose a name and a type (Infantry/Vehicle/All) for it<br>3. Click on a Loadout to load it and watch it happen on Battlelog or in game<br>4. Long click on a Loadout to remove it.</body></html");
+	public static final Instruction INSTRUCTION_OPTIONS=new Instruction("Loadout Saver",17,"<html><body>1. Enter your Battlog login information in the options menu, so the app can edit your loadout<br>2. Use the +-Button to save your currently equipped Loadout and choose a name  for it and select which parts of the Loadout should be saved (when sending a Loadout the missing pieces are supplemented with the corresponding pieces of your current loadout)<br>3. Click on a Loadout to load it and watch it happen on Battlelog or in game<br>4. Long click on a Loadout to remove it.</body></html");
 	public static final Instruction INSTRUCTION_BETA=new Instruction("Loadout Saver",17,"<html><body><b>Important notice</b><br>I can´t confirm that this app works for console players.<br>If it does thats great, if not, please don´t rate this app bad, but report the problem, so I can make it work<br>Thanks");
 	public static String getPlatformFromInt(int platform) {
 		switch(platform){
